90c006ed29512463f13436a19110300299d66cca
김승규 사원 <seungkyu.kim@dkbmc.com>
2022-08-29 09:57:47 +0900

    update comments

--- src/index.js
+++ src/index.js
@@ -24,24 +24,32 @@ class TodoItem {
  * @param {string} input - typed string
  * @returns {Object} TodoItem - new TodoItem Object
  *                            - generate a new id
- *                            - isDone = defatul (false)
- *                            - category = default (?)
- *                            - tags = []
+ *                            - default value
+ *                                contents: ""
+ *                                isDone: false
+ *                                category: string value
+ *                                tags: []
  */
-const createItem = (input) => {};
+const createItem = (input) => {
+  // create a new todo item...
+};
 
 /**
  * read an item
  * @param {string} id - id of selected item
  * @returns {Object} TodoItem - return an existing TodoItem Obejct whose id is matched with selected one
  */
-const readItem = (id) => {};
+const readItem = (id) => {
+  // read an item...
+};
 
 /**
  * read all items
  * @returns {Array} Array of TodoItem Obejcts - return all existing TodoItem Obejcts
  */
-const readAllItems = () => {};
+const readAllItems = () => {
+  // read all items
+};
 
 /**
  * update contents
@@ -49,7 +57,9 @@ const readAllItems = () => {};
  * @param {string} contents - will be updated if exist
  * @returns {Object} TodoItem - return updated TodoItem Object
  */
-const updateContents = (id, contents) => {};
+const updateContents = (id, contents) => {
+  // update contetns of the item
+};
 
 /**
  * update contents
@@ -57,7 +67,9 @@ const updateContents = (id, contents) => {};
  * @param {boolean} isDone - will be updated if exist
  * @returns {Object} TodoItem - return updated TodoItem Object
  */
-const updateIsDone = (id, isDone) => {};
+const updateIsDone = (id, isDone) => {
+  // update isDone of the item
+};
 
 /**
  * update contents
@@ -65,7 +77,9 @@ const updateIsDone = (id, isDone) => {};
  * @param {string} category - will be updated if exist
  * @returns {Object} TodoItem - return updated TodoItem Object
  */
-const updateCategory = (id, category) => {};
+const updateCategory = (id, category) => {
+  // update category of the item
+};
 
 /**
  * update contents
@@ -73,18 +87,24 @@ const updateCategory = (id, category) => {};
  * @param {Array} tags - will be updated if exist
  * @returns {Object} TodoItem - return updated TodoItem Object
  */
-const updateTag = (id, tag) => {};
+const updateTag = (id, tag) => {
+  // update tag of the item
+};
 
 /**
  * delete item => will fire readAllItems()
  * @param {string} id - id of selected item
  */
-const deleteItem = (id) => {};
+const deleteItem = (id) => {
+  // delete an item
+};
 
 /**
  * delete all items
  */
-const deleteAllItems = () => {};
+const deleteAllItems = () => {
+  // delete all items
+};
 
 /**
  * delete tag of selected item
@@ -92,11 +112,15 @@ const deleteAllItems = () => {};
  * @param {string} tag - tag that will be deleted
  * @returns {Object} TodoItem - return with updated tags array
  */
-const deleteTag = (id, tag) => {};
+const deleteTag = (id, tag) => {
+  // delete a tag of the item
+};
 
 /**
  * delete all tags of selected item
  * @param {string} id - id of selected item
  * @returns {Object} TodoItem - return with empty tags array
  */
-const deleteAllTags = (id) => {};
+const deleteAllTags = (id) => {
+  // delete all tags of the item
+};
